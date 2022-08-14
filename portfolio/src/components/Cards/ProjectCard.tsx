043ca76e73826a652e9beb709aa3707942a3cd5f
Alex McKinnon <mckinnonag@gmail.com>
2022-08-14 10:41:24 -0400

    Added chips to display language

--- portfolio/src/components/Cards/ProjectCard.tsx
+++ portfolio/src/components/Cards/ProjectCard.tsx
@@ -1,43 +1,38 @@
 import * as React from 'react';
+import Project from './Interfaces';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
-// Inserts a bullet
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
+// Inserts a chip to render languages and technologies in the card
+const chip = (
+    <Stack direction="row" spacing={1}>
+        <Chip label="Chip Outlined" variant="outlined" />
+    </Stack>
 );
 
-
-
-export interface Props {
-    title: string;
-    description: string;
-    languages: string[];
-    linkURL: string;
-}
-
-const ProjectCard = (props: Props) => {
+const ProjectCard = (props: Project) => {
   const technologies = props.languages.map((language) => 
-    <div>{language} </div>       
+    <>
+        <Chip label={language} variant="outlined" />
+    </>  
   );
 
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ minWidth: 275, margin: '10px', maxWidth: '5%' }}>
       <CardContent>
         <Typography variant="h5" component="div">
           {props.title}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {technologies}
+        <Stack direction="row" spacing={1}>
+            {technologies}
+        </Stack> 
         </Typography>
         <Typography variant="body2">
           {props.description}
--- portfolio/src/components/Cards/Projects.tsx
+++ portfolio/src/components/Cards/Projects.tsx
@@ -1,12 +1,38 @@
 import * as React from 'react';
 import ProjectCard from './ProjectCard';
+import Project from './Interfaces';
+import Stack from '@mui/material/Stack';
+
+const langsPortfolio: string[] = ['React', 'TypeScript'];
+const langsMoneyApp: string[] = ['React', 'TypeScript', 'Go', 'Postgres SQL'];
+const projects: Project[] = [
+    {
+        title: 'This Website',
+        description: 'A simple portfolio page built in TypeScript React.',
+        languages: langsPortfolio,
+        linkURL: 'google.com',
+    },
+    {
+        title: 'MoneyApp',
+        description: 'A expense-sharing application built with a TypeScript React frontend, a Go backend using Gin, Postgres SQL, and utilizing the Plaid API.',
+        languages: langsMoneyApp,
+        linkURL: 'google.com',
+    },
+];
+
+const cards = projects.map((project) =>
+    <ProjectCard title={project.title} 
+                 description={project.description} 
+                 languages={project.languages} 
+                 linkURL={project.linkURL} 
+    />
+);
 
 const Projects = () => {
     return (
         <>
-            <ProjectCard title="Project Title" description="description" languages={['Python', 'TypeScript']} linkURL='google.com' />
+            {cards}
         </>
-       
     )
 }
 
